3675e5a291effd3cc7262f666b0bd5cf735f7b34
Azhaguvel97 <82434204+Azhaguvel97@users.noreply.github.com>
2021-06-05 22:17:28 +0530

    Update College.java

--- src/org/univ/College.java
+++ src/org/univ/College.java
@@ -1,12 +1,15 @@
 package org.univ;
 
 public class College extends University {
+	// its is used to know abt the college info
 public void ug() {
 System.out.println("UG is B.E");	
 }
 public void pg() {
 	System.out.println("PG is M.E");
 }
+	public void ag(){
+	}
 public static void main(String[] args) {
 	College c=new College();
 	c.ug();
